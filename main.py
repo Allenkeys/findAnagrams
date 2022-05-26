f5a2ddc863d82406542deb6df42d15b2349403f9
Joshua <ezejoshuac@gmail.com>
2022-05-26 10:18:58 +0100

    Add split and sort methods to find_anagrams

--- main.py
+++ main.py
@@ -4,10 +4,26 @@
 # find_anagrams("racecar") --> True
 
 
-def find_anagrams(word):
+def find_anagrams(word, word1):
     # [assignment] Add your code here
-   
+    if(word.split().sort() == word1.split().sort() and len(word) == len(word1)):
+        return True
+    else:
+        return False   
 
-    return True
+# print(find_anagrams('one'))
 
-print(find_anagrams('one'))
+# aStr = 'mississippi'
+# alist = []
+
+# for letter in range(len(aStr)):
+#     alist.append(aStr[letter])
+
+
+
+# print(alist)
+# print(alist.count('s'))
+
+print(find_anagrams('listen', 'slite'))
+
+
